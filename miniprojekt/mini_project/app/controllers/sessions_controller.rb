a4040ceebefdcf4617719907f96532c2034641a3
Stephan <sbrink@tzi.de>
2013-02-18 17:10:32 +0100

    signin

--- miniprojekt/mini_project/app/controllers/sessions_controller.rb
+++ miniprojekt/mini_project/app/controllers/sessions_controller.rb
@@ -3,16 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email].downcase)
-    if user
-      sign_in user
-      redirect_to user
+    @user = User.find_by_email(params[:session][:email].downcase)
+    if @user
+      session[:user] = @user
+      redirect_to root_path
     else
-      flash.now[:error] = 'Invalid email/password combination' # Not quite right!      
+      flash.now[:error] = 'Invalid email/password combination'    
       render 'new'
     end
   end
 
   def destroy
+    if session[:user]
+      session.delete(:user)
+      redirect_to root_path
+    end
   end
 end
