ef38894cebac3bb4527f02750fb840dc780cfe11
Eike Behrends <eikebehr@tzi.de>
2013-02-18 15:08:43 +0100

    tests, funzen aber leider nicht :)

--- miniprojekt/mini_project/spec/requests/static_pages_spec.rb
+++ miniprojekt/mini_project/spec/requests/static_pages_spec.rb
@@ -11,5 +11,20 @@ describe "Static pages" do
     #it { should have_selector('title', text: full_title('')) }
     #it { should_not have_selector('title', text: '| Home') }
   end
-
+  
+  describe "link to new user" do
+    before { visit root_path }
+    #click_link "Registrieren"
+    page.should have_selector 'title', text: "Neuer User:"
+  end
+  
+  describe "Korrektes einloggen Testen" do
+    before do
+       visit root_path
+       @user = User.create(name: "Hodor", email: "hodor@hodor.de")
+       fill_in "Email", with: @user.email
+       click_button submit
+     end
+     it { should have_selector('title', text: "Hallo #{@user.name}") }
+   end
 end
