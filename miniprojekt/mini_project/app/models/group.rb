7dd122b4729d1fc532aa213bff55b28636f68d74
Stephan <sbrink@tzi.de>
2013-02-18 20:16:45 +0100

    beziehung project-group und group-user

--- miniprojekt/mini_project/app/models/group.rb
+++ miniprojekt/mini_project/app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ActiveRecord::Base
   attr_accessible :project_id
+
+  has_many :users
+  has_one  :project
 end
--- miniprojekt/mini_project/app/models/project.rb
+++ miniprojekt/mini_project/app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
   attr_accessible :description
+
+  belongs_to :group
 end
