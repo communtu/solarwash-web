af035f6cce46ba9671f13bdad7c12fd9ed110cca
Stephan <sbrink@tzi.de>
2013-02-18 14:17:22 +0100

    Tests fuer home

--- miniprojekt/mini_project/config/routes.rb
+++ miniprojekt/mini_project/config/routes.rb
@@ -1,7 +1,6 @@
 MiniProject::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  
+  root to: 'static_pages#home'
 
   resources :users
 
--- miniprojekt/mini_project/spec/requests/static_pages_spec.rb
+++ miniprojekt/mini_project/spec/requests/static_pages_spec.rb
@@ -1,11 +1,15 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
+describe "Static pages" do
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'AWE Benutzerverwaltung') }
+    #it { should have_selector('title', text: full_title('')) }
+    #it { should_not have_selector('title', text: '| Home') }
   end
+
 end
