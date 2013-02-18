d853ebf4fb7889f4a0308cee7c24fb6fbc207703
Eike Behrends <eikebehr@tzi.de>
2013-02-18 19:40:59 +0100

    user erweitert

--- miniprojekt/mini_project/app/models/user.rb
+++ miniprojekt/mini_project/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :description, :group_id
 
   before_save { email.downcase! }
 
--- miniprojekt/mini_project/db/schema.rb
+++ miniprojekt/mini_project/db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218164143) do
+ActiveRecord::Schema.define(:version => 20130218182148) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
+    t.integer  "group_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
