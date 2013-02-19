d85bccf4f70ed622790c9c94279f632cba3a2575
Eike Behrends <eikebehr@tzi.de>
2013-02-19 09:16:29 +0100

    dropdown menue

--- miniprojekt/mini_project/app/models/group.rb
+++ miniprojekt/mini_project/app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ActiveRecord::Base
   attr_accessible :project_id
 
   has_many :users
-  has_one  :project
+  has_one  :project, foreign_key: "project_id"
 end
--- miniprojekt/mini_project/db/schema.rb
+++ miniprojekt/mini_project/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218190500) do
+ActiveRecord::Schema.define(:version => 20130218192938) do
 
   create_table "groups", :force => true do |t|
     t.integer  "project_id"
