41e94e7f631c34dca6eb00555c80252f18949df2
Eike Behrends <eikebehr@tzi.de>
2013-02-19 15:33:01 +0100

    projekt erstellung

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awe09::Application.config.secret_token = 'e3e4d435d61f4f2e78f16d510aae69c6f3b6d761564a08edd2be1644dc18194761945758b45156a402a2931051ea6ec63cf9a51ba03484f67f9ee6c90614c79b'
