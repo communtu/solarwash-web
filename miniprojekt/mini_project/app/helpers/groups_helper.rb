dacc84468438342b7b5d1d178eedb3a45bee643b
Eike Behrends <eikebehr@tzi.de>
2013-02-19 15:18:01 +0100

    bootstrap eingebunden + gruppe nicht sichtbar, wenn voll

--- miniprojekt/mini_project/app/helpers/groups_helper.rb
+++ miniprojekt/mini_project/app/helpers/groups_helper.rb
@@ -1,2 +1,13 @@
 module GroupsHelper
+  
+  def possible_groups
+     Group.all.map do |g|
+       if(g.users.count < 2)
+         ["Gruppe #{g.id}",g.id]
+       else
+         nil
+       end
+     end
+  end
+  
 end
--- miniprojekt/mini_project/db/seeds.rb
+++ miniprojekt/mini_project/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email)
+  group = Group.create(project_id: 1)
+end
