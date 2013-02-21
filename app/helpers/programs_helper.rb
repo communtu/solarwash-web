076fe1a019669557015179cfa38d091fc90f4125
Stephan <sbrink@tzi.de>
2013-02-21 20:30:18 +0100

    fix in programs_of_device

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,10 +1,11 @@
 module ProgramsHelper
 	def programs_of_device device_id
-    Programs.all.map do |p|
-       if(p.device_id == device_id)
+    Device.all.map do |d|
+       if(d.progam_id == device_id)
          ["Gruppe #{g.id}",g.id]
        else
          nil
        end
+     end
 	end
 end
