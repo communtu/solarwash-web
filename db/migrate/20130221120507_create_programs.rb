353c237f23fed72b2d7dc4cb88f7143db6d9f924
Eike Behrends <eikebehr@tzi.de>
2013-02-21 20:41:28 +0100

    db modifiziert

--- db/migrate/20130221120507_create_programs.rb
+++ db/migrate/20130221120507_create_programs.rb
@@ -2,6 +2,7 @@ class CreatePrograms < ActiveRecord::Migration
   def change
     create_table :programs do |t|
       t.string :name
+      t.integer :device_id
       t.integer :degree
       t.integer :duration_in_min
       t.integer :consumption_in_wh
