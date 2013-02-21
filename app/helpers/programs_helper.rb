f43353eb03c05f0f2232160727ee88837e988cae
Stephan <sbrink@tzi.de>
2013-02-21 20:27:52 +0100

    auftrag anlegen formular ueberarbeitet

--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -1,2 +1,10 @@
 module ProgramsHelper
+	def programs_of_device device_id
+    Programs.all.map do |p|
+       if(p.device_id == device_id)
+         ["Gruppe #{g.id}",g.id]
+       else
+         nil
+       end
+	end
 end
