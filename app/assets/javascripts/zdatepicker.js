ba314aeaddf684a10c8dcfcfb962d6c055f3431b
Stephan <sbrink@tzi.de>
2013-02-23 12:36:06 +0100

    diverse views geaendert, datetimepicker mit konvertierung

---
+++ app/assets/javascripts/zdatepicker.js
@@ -0,0 +1,42 @@
+
+$.datepicker.regional['de'] = {
+	clearText: 'löschen', clearStatus: 'aktuelles Datum löschen',
+  closeText: 'schließen', closeStatus: 'ohne Änderungen schließen',
+  prevText: '<zurück', prevStatus: 'letzten Monat zeigen',
+  nextText: 'Vor>', nextStatus: 'nächsten Monat zeigen',
+  currentText: 'heute', currentStatus: '',
+  monthNames: ['Januar','Februar','März','April','Mai','Juni', 'Juli','August','September','Oktober','November','Dezember'],
+  monthNamesShort: ['Jan','Feb','Mär','Apr','Mai','Jun', 'Jul','Aug','Sep','Okt','Nov','Dez'],
+  monthStatus: 'anderen Monat anzeigen', yearStatus: 'anderes Jahr anzeigen',
+  weekHeader: 'Wo', weekStatus: 'Woche des Monats',
+  dayNames: ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag'],
+  dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+  dayNamesMin: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+  dayStatus: 'Setze DD als ersten Wochentag', dateStatus: 'Wähle D, M d',
+  dateFormat: 'dd.mm.yy', firstDay: 1, 
+  initStatus: 'Wähle ein Datum', isRTL: false};
+$.datepicker.setDefaults($.datepicker.regional['de']);
+
+$.timepicker.regional['de'] = {
+	timeOnlyTitle: 'Zeit Wählen',
+	timeText: 'Zeit',
+	hourText: 'Stunde',
+	minuteText: 'Minute',
+	secondText: 'Sekunde',
+	millisecText: 'Millisekunde',
+	timezoneText: 'Zeitzone',
+	currentText: 'Jetzt',
+	closeText: 'Fertig',
+	timeFormat: 'HH:mm',
+	amNames: ['vorm.', 'AM', 'A'],
+	pmNames: ['nachm.', 'PM', 'P'],
+	isRTL: false
+};
+$.timepicker.setDefaults($.timepicker.regional['de']);
+
+$(document).ready(function(){
+  $('input.ui-date-picker').datepicker();
+  $('input.ui-datetime-picker').datetimepicker({
+  	useLocalTimezone: true
+  });
+});
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,8 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all
+    @device = Device.find(params[:device_id])
+    @job = @device.jobs.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +14,8 @@ class JobsController < ApplicationController
   # GET /jobs/1
   # GET /jobs/1.json
   def show
-    @job = Job.find(params[:id])
+    @device = Device.find(params[:device_id])
+    @job = @device.jobs.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -24,7 +26,7 @@ class JobsController < ApplicationController
   # GET /jobs/new
   # GET /jobs/new.json
   def new
-    @device = Device.find(params[:device_id])
+    @device = Device.find(params[:device_id])    
     @job = @device.jobs.new
 
     respond_to do |format|
@@ -35,7 +37,8 @@ class JobsController < ApplicationController
 
   # GET /jobs/1/edit
   def edit
-    @job = Job.find(params[:id])
+    @device = Device.find(params[:device_id])
+    @job = @device.jobs.find(params[:id])
   end
 
   # POST /jobs
@@ -44,10 +47,12 @@ class JobsController < ApplicationController
     @device = Device.find(params[:device_id])
     @job = @device.jobs.new(params[:job])
 
+    @job.end_of_timespan = DateTime.strptime(params[:job]['end_of_timespan'], '%d.%m.%Y %H:%M')
+
     respond_to do |format|
 
       if @job.save
-        format.html { redirect_to [@device, @job], notice: 'Job was successfully created.' }
+        format.html { redirect_to [@device, @job], notice: 'Auftrag wurde erfolgreich angelegt.' }
         format.json { render json: @job, status: :created, location: @job }
       else
         format.html { render action: "new"}
@@ -59,11 +64,12 @@ class JobsController < ApplicationController
   # PUT /jobs/1
   # PUT /jobs/1.json
   def update
-    @job = Job.find(params[:id])
+    @device = Device.find(params[:device_id])
+    @job = @device.jobs.find(params[:id])
 
     respond_to do |format|
       if @job.update_attributes(params[:job])
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+        format.html { redirect_to @job, notice: 'Auftrag wurde erfolgreich geändert.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -75,7 +81,8 @@ class JobsController < ApplicationController
   # DELETE /jobs/1
   # DELETE /jobs/1.json
   def destroy
-    @job = Job.find(params[:id])
+    @device = Device.find(params[:device_id])
+    @job = @device.jobs.find(params[:id])
     @job.destroy
 
     respond_to do |format|
