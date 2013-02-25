88da1ccd395d96157fd313e9a4de3bde52ec8301
Stephan <sbrink@tzi.de>
2013-02-25 12:46:21 +0100

    devise, developer bar, zeit simulieren

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+class SessionsController < ApplicationController
+
+  before_filter :check_development_mode!
+
+  def create
+    Timecop.freeze(params[:simulate_datetime])
+    flash[:success] = "Zeit gesetzt auf #{params[:simulate_datetime]}."
+
+    user_id = params[:user_id]
+
+    if user = User.where(id: user_id).first
+      sign_in(:user, user)
+      flash[:success] = "Angemeldet als #{user.email}."
+      redirect_to :root
+    else
+      flash[:error] = "Ungültige Benutzer-ID (#{user_id})."
+      redirect_to :back
+    end
+  end
+
+  protected
+
+  def check_development_mode!
+    unless development_mode?
+      flash[:error] = "Nicht im Development Mode."
+      redirect_to :back
+    end
+  rescue ActionController::RedirectBackError
+    redirect_to root_path
+  end
+
+end
