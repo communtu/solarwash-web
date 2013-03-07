5e963d20b631c188d79167d3d6109cf59aac0729
Stephan <sbrink@tzi.de>
2013-03-07 11:55:37 +0100

    user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,5 +20,44 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {
+    @user = User.new(name: "Stephan Brinkmann", email: "sbrink@tzi.de", password: '12345678', password_confirmation: '12345678', apartmentnumber:1)
+    #user.skip_confirmation! 
+    #user.save!
+  }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:apartmentnumber) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is not present" do
+    before { @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when apartment number is not present" do
+    before { @user.apartmentnumber = " " }
+    it { should_not be_valid }
+  end
 end
