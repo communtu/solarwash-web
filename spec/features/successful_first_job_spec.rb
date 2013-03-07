e3dc81a41b425e2be75d081b7a0c473fdae69130
Stephan <sbrink@tzi.de>
2013-03-07 10:56:01 +0100

    before-block fuer ganze spec-klasse

--- spec/features/successful_first_job_spec.rb
+++ spec/features/successful_first_job_spec.rb
@@ -3,7 +3,8 @@
 
 require 'spec_helper'
 
-describe "Sonnenzeit: Jetzt + 4 Stunden - Erster Job fuer das Device:", :type => :feature do
+describe "Erster Auftrag fuer das Device", :type => :feature do
+
   before :each do
     #TODO : Auslagern
     user = User.new(  name: "Stephan Brinkmann",
@@ -23,9 +24,7 @@ describe "Sonnenzeit: Jetzt + 4 Stunden - Erster Job fuer das Device:", :type =>
     
     Device.create(name: "Miele Keller 1", devicetype: 0, state: 0)
     Program.create(name: "Pflegeleicht", degree: "40", duration_in_min: 180, consumption_in_wh: 560, device_id: 1)
-    
-    Setting.create(sun_hour: DateTime.now.hour+4, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
-    
+        
     visit new_device_job_path(Device.find(1))
     
     page.should have_content 'Miele Keller 1'
@@ -35,116 +34,71 @@ describe "Sonnenzeit: Jetzt + 4 Stunden - Erster Job fuer das Device:", :type =>
     choose('Yes')
   end
 
-  it "Job muss spätestens in 3 Stunden fertig sein" do
-
-    fill_in "Fertig bis spätestens", with: (DateTime.now + 3.hour)
+  describe "Sonnenzeit: Jetzt + 4 Stunden - Erster Job fuer das Device:", :type => :feature do
     
-    click_button 'Auftrag erstellen'
-    current_path.should == root_path
-    assert_equal(DateTime.now, Job.find(1).start.to_datetime)
-    assert_equal(2, Device.find(1).state)
-  end
-  
-  it "Job muss spätestens in 10 Stunden fertig sein" do
-    fill_in "Fertig bis spätestens", with: (DateTime.now + 10.hour) 
-    click_button 'Auftrag erstellen'
-    current_path.should == root_path
-    
-    assert_equal(DateTime.now.change({hour: DateTime.now.hour+4, min: DateTime.now.min, sec: DateTime.now.sec}), Job.find(1).start.to_datetime)
-    assert_equal(1, Device.find(1).state)
-  end
-  
-  it "Job muss spätestens in 5 Stunden fertig sein" do
-    fill_in "Fertig bis spätestens", with: (DateTime.now + 5.hour)
-    click_button 'Auftrag erstellen'
-    current_path.should == root_path
-    
-    assert_equal(DateTime.now.change({hour: DateTime.now.hour+2, min: DateTime.now.min, sec: DateTime.now.sec}), Job.find(1).start.to_datetime)
-    assert_equal(1, Device.find(1).state)
-  end
-end
-
-describe "Sonnenzeit: Jetzt -1 Stunde - Erster Job fuer das Device:", :type => :feature do
     before :each do
-      #TODO : Auslagern
-      user = User.new(  name: "Stephan Brinkmann",
-                        email: "sbrink@tzi.de",
-                        password: '12345678', 
-                        password_confirmation: '12345678',
-                        apartmentnumber: 1)
-      user.skip_confirmation! 
-      user.save!
-
-      visit "/users/sign_in"
-
-      fill_in "Email",    :with => "sbrink@tzi.de"
-      fill_in "Passwort", :with => "12345678"
-
-      click_button "Anmelden"
-
-      Device.create(name: "Miele Keller 1", devicetype: 0, state: 0)
-      Program.create(name: "Pflegeleicht", degree: "40", duration_in_min: 180, consumption_in_wh: 560, device_id: 1)
-
-      Setting.create(sun_hour: DateTime.now.hour-1, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
-
-      visit new_device_job_path(Device.find(1))
-
-      page.should have_content 'Miele Keller 1'
-      page.should have_content 'für das Gerät Miele Keller 1'
-      assert_equal(0, Device.find(1).state)
-      select('Pflegeleicht, 40° (180 Minuten)', :from => 'Program')
-      choose('Yes')
-    end
+      Setting.create(sun_hour: DateTime.now.hour+4, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
+    end      
 
     it "Job muss spätestens in 3 Stunden fertig sein" do
+
       fill_in "Fertig bis spätestens", with: (DateTime.now + 3.hour)
+      
+      click_button 'Auftrag erstellen'
+      current_path.should == root_path
+      assert_equal(DateTime.now, Job.find(1).start.to_datetime)
+      assert_equal(2, Device.find(1).state)
+    end
+    
+    it "Job muss spätestens in 10 Stunden fertig sein" do
+      fill_in "Fertig bis spätestens", with: (DateTime.now + 10.hour) 
       click_button 'Auftrag erstellen'
       current_path.should == root_path
       
+      assert_equal(DateTime.now.change({hour: DateTime.now.hour+4, min: DateTime.now.min, sec: DateTime.now.sec}), Job.find(1).start.to_datetime)
+      assert_equal(1, Device.find(1).state)
+    end
+    
+    it "Job muss spätestens in 5 Stunden fertig sein" do
+      fill_in "Fertig bis spätestens", with: (DateTime.now + 5.hour)
+      click_button 'Auftrag erstellen'
+      current_path.should == root_path
+      
+      assert_equal(DateTime.now.change({hour: DateTime.now.hour+2, min: DateTime.now.min, sec: DateTime.now.sec}), Job.find(1).start.to_datetime)
+      assert_equal(1, Device.find(1).state)
+    end
+  end
+
+  describe "Sonnenzeit: Jetzt -1 Stunde - Erster Job fuer das Device:", :type => :feature do
+      
+      before :each do
+        Setting.create(sun_hour: DateTime.now.hour-1, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
+      end 
+
+      it "Job muss spätestens in 3 Stunden fertig sein" do
+        fill_in "Fertig bis spätestens", with: (DateTime.now + 3.hour)
+        click_button 'Auftrag erstellen'
+        current_path.should == root_path
+        
+        assert_equal(2, Device.find(1).state)
+        assert_equal(DateTime.now, Job.find(1).start.to_datetime)
+      end
+  end
+
+  describe "Sonnenzeit: Jetzt -3 Stunden - Erster Job fuer das Device:", :type => :feature do
+
+    before :each do
+      Setting.create(sun_hour: DateTime.now.hour-3, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
+    end 
+
+    it "Job muss spätestens in 5 Stunden  Uhr fertig sein" do   
+      fill_in "Fertig bis spätestens", with: (DateTime.now + 5.hour)
+      click_button 'Auftrag erstellen'
+      current_path.should == root_path
+        
       assert_equal(2, Device.find(1).state)
       assert_equal(DateTime.now, Job.find(1).start.to_datetime)
     end
-end
-
-describe "Sonnenzeit: Jetzt -3 Stunden - Erster Job fuer das Device:", :type => :feature do
-  before :each do
-    #TODO : Auslagern
-    user = User.new(  name: "Stephan Brinkmann",
-                      email: "sbrink@tzi.de",
-                      password: '12345678', 
-                      password_confirmation: '12345678',
-                      apartmentnumber: 1)
-    user.skip_confirmation! 
-    user.save!
-
-    visit "/users/sign_in"
-
-    fill_in "Email",    :with => "sbrink@tzi.de"
-    fill_in "Passwort", :with => "12345678"
-
-    click_button "Anmelden"
-
-    Device.create(name: "Miele Keller 1", devicetype: 0, state: 0)
-    Program.create(name: "Pflegeleicht", degree: "40", duration_in_min: 180, consumption_in_wh: 560, device_id: 1)
-
-    Setting.create(sun_hour: DateTime.now.hour-3, sun_minute: DateTime.now.min, sun_second: DateTime.now.sec , time_to_confirm: 5)
-
-    visit new_device_job_path(Device.find(1))
-
-    page.should have_content 'Miele Keller 1'
-    page.should have_content 'für das Gerät Miele Keller 1'
-    assert_equal(0, Device.find(1).state)
-    select('Pflegeleicht, 40° (180 Minuten)', :from => 'Program')
-    choose('Yes')
   end
 
-  it "Job muss spätestens in 5 Stunden  Uhr fertig sein" do   
-    fill_in "Fertig bis spätestens", with: (DateTime.now + 5.hour)
-    click_button 'Auftrag erstellen'
-    current_path.should == root_path
-      
-    assert_equal(2, Device.find(1).state)
-    assert_equal(DateTime.now, Job.find(1).start.to_datetime)
-  end
 end
-
